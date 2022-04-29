c8a11ae6493529cc5d6e9def29714ea1a8cdf84f
Aleksa Balgojević <skr.aleksa@gmail.com>
2022-04-29 17:47:58 +0200

    Renamed Projects to projects

--- Projects/ProjectEuler/Problem17.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main (int argc, char *argv[])
-{
-    printf("%d\n",102 * 99 + 11 + 9 * (147+8*36));
-    return 0;
-}
--- Projects/The Practice of Programming/Design and implementation/Markov/MarkovJava/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -cp '/home/baleksa/coding/Projects/The Practice of Programming/Design and implementation/Markov/MarkovJava' Markov
