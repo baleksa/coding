c59b82d7e4e892864628aa9944796106614123f8
Aleksa Balgojević <skr.aleksa@gmail.com>
2022-05-01 20:23:07 +0200

    update

--- projects/The Practice of Programming/Interfaces/CSV Library/test_performance.sh
+++ projects/The Practice of Programming/Interfaces/CSV Library/test_performance.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
-
-for i in 0 1 2 3; do
-	case "$i" in
-	0)
-		echo "GREEDY:"
-		;;
-	1)
-		echo "LAZY_ALL:"
-		;;
-	2)
-		echo "LAZY_UP_TO_N:"
-		;;
-	3)
-		echo "LAZY_ONLY_N:"
-		;;
-	esac
-	/bin/time -p cat './test/data/csv/1000000 Sales Records.csv' | ./test_multifield "$i" "0" 1>/dev/null
+for tfile in ./test/data/csv/*; do
+    echo "Reading from file $tfile:"
+	for i in 0 1 2 3; do
+		indent="  "
+		case "$i" in
+		0)
+			printf "%sGREEDY: " "$indent"
+			;;
+		1)
+			printf "%sLAZY_ALL: " "$indent"
+			;;
+		2)
+			printf "%sLAZY_UP_TO_N: " "$indent"
+			;;
+		3)
+			printf "%sLAZY_ONLY_N: " "$indent"
+			;;
+		esac
+		/bin/time -f%e cat "$tfile" | ./test_multifield "$i" "0" 1>/dev/null
+	done
 done
