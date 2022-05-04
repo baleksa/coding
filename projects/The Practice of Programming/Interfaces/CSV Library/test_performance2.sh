43596c375acf1690e713fbde19e58e650d778ee0
Aleksa Balgojević <skr.aleksa@gmail.com>
2022-05-04 11:19:01 +0200

    Update commit

---
+++ projects/The Practice of Programming/Interfaces/CSV Library/test_performance2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+for tfile in ./data/csv/*; do
+    echo "Reading from file $tfile:"
+	for i in 0 1 2 3; do
+		indent="  "
+                printf "%s" "$indent"
+		case "$i" in
+		0)
+			printf "GREEDY: "
+			;;
+		1)
+			printf "LAZY_ALL: "
+			;;
+		2)
+			printf "LAZY_UP_TO_N: "
+			;;
+		3)
+			printf "LAZY_ONLY_N: "
+			;;
+		esac
+		/bin/time -f%e cat "$tfile" | ./test_multifield "$i" "0" 1>/dev/null
+	done
+done
